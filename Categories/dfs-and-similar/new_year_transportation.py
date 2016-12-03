4d5708e4f01992fcbf79828560b2350aa9c0855d
Mai Truong <maitruong2801@gmail.com>
2016-12-02 22:13:04 -0800

    Add code for new year transportation

--- Categories/dfs-and-similar/new_year_transportation.py
+++ Categories/dfs-and-similar/new_year_transportation.py
@@ -1,1 +1,12 @@
-
+# http://ideone.com/EitERj
+n, t = map(int, raw_input().split())
+a = map(int, raw_input().split())
+cur = 1
+cell = []
+while cur < n:
+	cell.append(cur)
+	cur = cur + a[cur-1]
+cell.append(cur)
+print cell
+if t in cell: print 'YES' 
+else: print 'NO'
