59c289c54eaec6562f7a50a82452eebb6b6fc68f
Mai Truong <maitruong2801@gmail.com>
2016-12-11 21:09:00 -0800

    Delete the_new_year_meeting_friends.py

--- Contests/the_new_year_meeting_friends.py
+++
@@ -1,7 +0,0 @@
-coor = map(int, raw_input().split())
-maxC = max(coor[0], max(coor[1], coor[2]))
-minD = 300
-for i in range(maxC):
-    if minD > abs(coor[0] - i) + abs(coor[1] - i) + abs(coor[2] - i):
-        minD = abs(coor[0] - i) + abs(coor[1] - i) + abs(coor[2] -i)
-print minD
