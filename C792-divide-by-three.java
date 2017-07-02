942edd0f2bdf88b00472cd528b05b1f081c8dc1a
Mai Truong <maitruong2801@gmail.com>
2017-07-02 11:24:37 -0700

    Update C792-divide-by-three.java

--- C792-divide-by-three.java
+++ C792-divide-by-three.java
@@ -1,21 +1,37 @@
 public class Main {
     public int divideByThree(long n) {
         char[] nums = String.valueOf(n).toCharArray();
-        int[] dp = new int[nums.length];
+        int[] re = new int[3];
         int sum = 0;
         for (int i = 0; i < nums.length; i++) {
-            sum += nums[i] - '0';
-            if (sum % 3 == 0) {
-                dp[i] = 0;
-            } else {
-                dp[i] = dp[i-1] + 1;
-            }
+            re[(nums[i] - '0') % 3]++;
         }
-        int cnt = dp[n.length-1];
-        for (int i = 0; dp[i] == 0; i++) cnt++;
-        if (cnt >= n.length) return -1;
-        else return cnt;
-        // 10011001100110001
+        String single = "", pair = "";
+        sum = (re[1] + re[2]*2) % 3;
+        if (sum == 0) return n;
+        else if (sum == 1) {
+            if (re[1] > 0) single = remove(String.valueOf(n), 1, 1);
+            if (re[2] > 1) pair = remove(String.valueOf(n), 2, 2);
+        } else if (sum == 2) {
+            String single = "", pair = "";
+            if (re[1] > 2) single = remove(String.valueOf(n), 1, 2);
+            if (re[2] > 0) pair = remove(String.valueOf(n), 2, 1);
+        }
+        if (single.isEmpty() && pair.isEmpty()) return -1;
+        else if (!single.isEmpty() && !pair.isEmpty()) return single.length() > pair.length() ? Long.valueOf(pair) : Long.valueOf(single);
+        else if (single.isEmpty()) return Long.valueOf(pair);
+        else if (pair.isEmpty()) return Long.valueOf(single);
+    }
+    public String remove(String n, int remain, int cnt) {
+        StringBuilder res = new StringBuilder();
+        for (int i = n.length()-1; i >= 0; i--) {
+            if ((n.charAt(i) - '0') % 3 == remain && cnt > 0) cnt--;
+            else res.append(n.charAt(i));
+        }
+        res = res.reverse();
+        i = 0;
+        while (i < res.length() && res.charAt(i) == '0') i++;
+        return res.substring(i).toString();
     }
     public static void main(String args[]) {
         
