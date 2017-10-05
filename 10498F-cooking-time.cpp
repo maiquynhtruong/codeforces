272c79b1297b4355e9ea38ac0a3ce9274c19ef7b
Mai Truong <maitruong2801@gmail.com>
2017-10-05 15:01:01 -0400

    Update 10498F-cooking-time.cpp

--- 10498F-cooking-time.cpp
+++ 10498F-cooking-time.cpp
@@ -1,27 +1,40 @@
-#include <iostream>
+#include <bits/stdc++.h> 
+#define LL long long
 using namespace std;
-int test, n, k, a[100005];
+int test, n, k, d;
+LL a[100005];
 int main() {
 	cin >> test;
 	while (test--) {
 		cin >> n >> k;	
 		int ans = k;
-		set<int> next, cur;
+		set<LL> next, cur;
 		for (int i = 1; i <= n; i++) {
 			cin >> a[i];
-			cur.insert(a[i]);
 		}
-		// cur.insert(a[1]);
-		for (int i = 2; i <= k; i++) {
-			next.insert(a[i+k]);
-		}
-		vector<int> diff;
+		for (int i = 1; i <= k; i++) { cur.insert(a[i]);}
+		for (int i = k+2; i <= n; i++) { next.insert(a[i]);}
+		set<LL> diff;
+ 
 		for (int i = k+1; i <= n; i++) {
-			set_difference(cur, cur+k, next, next+n, diff.begin());
-			
+			// cout << a[i] << "\n";
+			next.erase(a[i]); if (i+k <= n) next.insert(a[i+k]); //diff.clear();
+			// cout << "cur: "; for (set<LL>::iterator it = cur.begin(); it != cur.end(); it++) { cout << *it << " ";} cout << "\n";
+			// cout << "next: "; for (set<LL>::iterator it = next.begin(); it != next.end(); it++) { cout << *it << " ";} cout << "\n";
+			if (cur.find(a[i]) == cur.end()) {
+				// set_difference(cur.begin(), cur.end(), next.begin(), next.end(), inserter(diff, diff.end()));
+				// if (!diff.empty()) d = *(diff.begin()); else d = *(cur.end());
+				d = *(cur.end());
+				for (set<LL>::iterator it = cur.begin(); it != cur.end(); it++) {
+					if (next.find(*it) == next.end()) { d = *it; break;}
+				}
+				// cout << "diff: "; for (set<LL>::iterator it = diff.begin(); it != diff.end(); it++) { cout << *it << " ";} cout << "\n";
+				cur.erase(d); cur.insert(a[i]);
+				ans++;
+			}
 		}
-		
-		
+ 
+		cout << ans << "\n";
 	}
 	return 0;
 }
